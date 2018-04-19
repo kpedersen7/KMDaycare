117035c6fcd419faaf6505dbc0ae771eb2c7e083
Rinki Mistry <rinki114@hotmail.com>
2018-04-19 15:59:02 -0600

    evnet test

--- KMDaycare-Website/AddEvent.aspx.cs
+++ KMDaycare-Website/AddEvent.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -178,6 +179,46 @@ public partial class AddEvent : System.Web.UI.Page
             }
         }
         SetCookie();
+
+        string[] NewdateArray = {"6:00 AM",
+"6:30 AM",
+"7:00 AM",
+"7:30 AM",
+"8:00 AM",
+"8:30 AM",
+"9:00 AM",
+"9:30 AM",
+"10:00 AM",
+"10:30 AM",
+"11:00 AM",
+"11:30 AM",
+"12:00 PM",
+"12:30 PM",
+"1:00 PM",
+"1:30 PM",
+"2:00 PM",
+"2:30 PM",
+"3:00 PM",
+"3:30 PM",
+"4:00 PM",
+"4:30 PM",
+"5:00 PM"
+};
+        List<string> dateList = new List<string>();
+
+        
+            foreach (Event ev in eventsForDay)
+            {
+
+                    string aDate = ev.StartDateTime.ToString("h:mm tt");
+                    NewdateArray = NewdateArray.Where(w => w != aDate).ToArray();
+            }
+        
+       
+
+
+        
+
     }
 
     private void RemoveEvent(int id)
